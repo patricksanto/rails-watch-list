169a9d93e80f44d3894b3a8e60ffde608793649a
Patrick Santos <patrick.sg.santos@gmail.com>
2023-02-28 10:13:34 -0300

    trying some modifications

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,3 +1,6 @@
+require "json"
+require "open-uri"
+
 class ListsController < ApplicationController
   def index
     @lists = List.all
@@ -9,6 +12,16 @@ class ListsController < ApplicationController
 
   def new
     @list = List.new
+    url = "https://api.themoviedb.org/3/movie/45?api_key=3adb77a7d9d67af81bb3f72ac50f08eb"
+    movies_serialized = URI.open(url).read
+    movies = JSON.parse(movies_serialized)
+    teste = movies.keys
+    teste2 = movies[0]m["results"]
+    movies.each do |key, result|
+    p key
+    end
+    raise
+    puts "#{user["name"]} - #{user["bio"]}"
   end
 
   def create
